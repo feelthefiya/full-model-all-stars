d812dd77a54976f3518d61192099ab2747d45512
feelthefiya <patrickwalshee67@outlook.com>
2023-07-07 12:04:13 +0100

    email-edit

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST{'email'};
 $subject = $_POST{'subject'};
 $message = $_POST{'message'};
 
-$email_from = 'info@bnafallstars.com';
+$email_from = 'no-reply@bnafallstars.com';
 
 $email_subject = 'New Form Submission';
 
